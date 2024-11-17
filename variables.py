ff5c2f0f20ae39e828d393cd2c7c4068645fa69d
Nordavind19 <masecret19@gmail.com>
2024-11-17 11:12:33 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,7 @@
+
+number_of_completed_homework = 12
+spent_time = 1.5
+course_name = 'Python'
+time_for_one_task = (spent_time/number_of_completed_homework)
+print ("Курс:", course_name, "всего задач:", number_of_completed_homework, ",затрачено часов:", spent_time, ",среднее время выполнения:", time_for_one_task
+       )
