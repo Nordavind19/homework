08d8e9592a31598c2ad0b4cabfcb230f0aacb021
Nordavind19 <masecret19@gmail.com>
2024-11-22 22:58:22 +0300

    Add files via upload

---
+++ module_1_5.py.py
@@ -0,0 +1,9 @@
+immutable_var = (5, 6, 20, 'word')
+print(immutable_var)
+
+mutable_list = [8, 12, 'music', 'book', True]
+print(mutable_list)
+mutable_list[0] = 33
+print(mutable_list)
+mutable_list[2] = 'winter'
+print(mutable_list)
