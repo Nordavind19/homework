b060285ac77099d0a9b6473775e8b4b4b7a0a3aa
Nordavind19 <masecret19@gmail.com>
2024-11-26 11:27:17 +0300

    Add files via upload

---
+++ module_1_6.py.py
@@ -0,0 +1,15 @@
+
+my_dict={'Kate': 2000, 'Steven': 1998, 'Andy': 2004}
+print(my_dict)
+print(my_dict['Steven'])
+print(my_dict.get('Ann'))
+my_dict.update({'Jane': 2001, 'John': 2006})
+deleted_key=my_dict.pop('Andy')
+print(deleted_key)
+print(my_dict)
+my_set={2, 3, 7, 8, 2, 5, 7, 'book', 'textbook', 'book'}
+print(my_set)
+print(my_set.add(10))
+print(my_set.add('winter'))
+print(my_set.discard(8))
+print(my_set)
